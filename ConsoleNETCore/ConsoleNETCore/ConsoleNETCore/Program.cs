ec3ef68078178db703cafea36b01227ae50fb206
Amando Jr <amando.jr@gmail.com>
2019-06-25 20:19:00 -0300

    ConsoleApp NetCore
    ConsoleApp NetCore Docker Ready

---
+++ ConsoleNETCore/ConsoleNETCore/ConsoleNETCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleNETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo!");
+            Console.Write("Informe o ano: ");
+            var ano = Console.ReadLine();
+            int valorAno = int.Parse(ano);
+            int idade = DateTime.Today.Year - valorAno;
+            Console.WriteLine($"\r\nSua idade: {idade}");
+        }
+    }
+}
+
